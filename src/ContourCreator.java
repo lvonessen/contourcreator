e4e7fb0396c03bea8b84e3ac21e6ae99d01330b5
Laura Vonessen <laurav4@cs.washington.edu>
2018-05-27 11:53:04 -0700

    Consolidated topo paths.

--- src/ContourCreator.java
+++ src/ContourCreator.java
@@ -9,12 +9,12 @@ import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.awt.geom.AffineTransform;
+import java.awt.geom.Line2D;
 import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
 import java.io.IOException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.JFrame;
@@ -23,16 +23,19 @@ import javax.swing.JPanel;
 public class ContourCreator extends JPanel {
 
 	private List<Shape> contours;
-	private List<Shape> transformedContours;
 	private List<Shape> majorContours;
-	private List<Shape> transformedMajorContours;
-	private Path2D.Double contour;
-	private List<Triangle> mesh;
 	private AffineTransform zoom = new AffineTransform();
 	private AffineTransform drag = new AffineTransform();
+	
+	Point2D.Double pt = new Point2D.Double(0, 0);
 
 	private double lakeWashThresh = 4.06;
-	private double[] seattleMajorVals = { 3, lakeWashThresh, 4.5395, 5.01, 5.45};
+	private double[] seattleMajorVals = { 3, //
+			lakeWashThresh, //
+			4.5395, 5.01, 5.35 }; // 5.45
+	private Color[] seattleColors = { Color.BLACK, //
+			Color.BLACK, //
+			Color.RED, Color.MAGENTA, Color.BLUE };
 
 	public static void main(String[] args) throws IOException {
 
@@ -49,43 +52,37 @@ public class ContourCreator extends JPanel {
 	}
 
 	public ContourCreator(String file) throws IOException {
+
+		TopoMap tm = new TopoMap(file);
+
 		double maxHeight = 6;// 1.5;
 		double stepSize = .1;
-		double[] majorVals = seattleMajorVals;
 
 		contours = new ArrayList<Shape>();
-		transformedContours = new ArrayList<Shape>();
 		majorContours = new ArrayList<Shape>();
-		transformedMajorContours = new ArrayList<Shape>();
 
-		Path stlPath = Paths.get(file);
-
-		mesh = STLParser.parseSTLFile(stlPath);
-
-		double[][] bounds = getBounds(mesh);
+		double[][] bounds = tm.getBounds();
+		System.out.println(Arrays.toString(bounds[0]));
+		System.out.println(Arrays.toString(bounds[1]));
 
 		AffineTransform affine = new AffineTransform();
-		// affine.translate(500,500);
 
+		// this mirrors the map so it's right side up, etc
 		affine.scale(3, 3);
-
 		affine.translate(0, bounds[1][1]);
-
 		affine.scale(1, -1);
 
-		for (double thresh = 4; /* 0.0000000000001; */thresh < maxHeight; thresh += stepSize) {
-			Path2D.Double path = process(thresh);
-			// System.out.println();
-			contours.add(affine.createTransformedShape(path));
-			transformedContours.add(affine.createTransformedShape(path));
-			// contours[i
+		tm.initialize(seattleMajorVals);
+		List<Path2D.Double> cts = tm.asPaths();
+		for (Path2D.Double ct : cts) {
+			majorContours.add(affine.createTransformedShape(ct));
 		}
 
-		for (double thresh : majorVals) {
-			Path2D.Double path = process(thresh);
-			// System.out.println();
-			majorContours.add(affine.createTransformedShape(path));
-			transformedMajorContours.add(affine.createTransformedShape(path));
+		// get minor vals
+		tm.initialize(4, maxHeight, stepSize);
+		cts = tm.asPaths();
+		for (Path2D.Double ct : cts) {
+			contours.add(affine.createTransformedShape(ct));
 		}
 
 		addMouseWheelListener(new ZoomListener());
@@ -98,15 +95,19 @@ public class ContourCreator extends JPanel {
 		Graphics2D g2 = (Graphics2D) g;
 
 		boolean col = true;
-		g2.setColor(Color.RED);
-		for (Shape co : transformedContours) {
+		g2.setColor(Color.LIGHT_GRAY);
+		for (Shape co : contours) {
 			g2.draw(co);// zoom.createTransformedShape(drag.createTransformedShape(co)));
 		}
 		g2.setColor(Color.BLACK);
 		g2.setStroke(new BasicStroke(2));
-		for (Shape co : transformedMajorContours) {
-			g2.draw(co);// zoom.createTransformedShape(drag.createTransformedShape(co)));
+		for (int i = 0; i < majorContours.size(); i++) {
+			Shape co = majorContours.get(i);
+			g2.setColor(seattleColors[i]);
+			g2.draw(co);
 		}
+		
+		drawWithColor(g2, Color.BLACK, pt);
 	}
 
 	private Color toggle(boolean tog) {
@@ -117,113 +118,15 @@ public class ContourCreator extends JPanel {
 		}
 	}
 
-	private double[][] getBounds(List<Triangle> mesh) {
-		double[] min = { Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE };
-		double[] max = { Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE };
-		for (Triangle t : mesh) {
-			for (Vec3d vertex : t.getVertices()) {
-				double[] array = getArray(vertex);
-				for (int i = 0; i < 3; i++) {
-					if (array[i] < min[i]) {
-						min[i] = array[i];
-					}
-					if (array[i] > max[i]) {
-						max[i] = array[i];
-					}
-				}
-			}
-		}
-		return new double[][] { min, max };
-	}
-
-	private double[] getArray(Vec3d pt) {
-		return new double[] { pt.x, pt.y, pt.z };
-	}
-
-	private Path2D.Double process(double zThresh) throws IOException {
-		contour = new Path2D.Double();
-		double almostZero = 0.0000;
-
-		for (Triangle t : mesh) {
-			int countBelow = 0, countAbove = 0, countAt = 0;
-			Vec3d prev = null;
-
-			// special lake washington filter
-			if (zThresh == lakeWashThresh) {
-				if (t.getVertices()[0].x < 110) {
-					continue;
-				}
-			}
-
-			for (Vec3d vertex : t.getVertices()) {
-
-				if ((vertex.z - zThresh) * (vertex.z - zThresh) <= almostZero) {
-					// not sure this is the right thing to do for the "equals" case
-					countAt++;
-				} else if (vertex.z < zThresh) {
-					countBelow++;
-				} else {
-					countAbove++;
-				}
-				prev = vertex;
-			}
-
-			List<Vec3d> list = new ArrayList<Vec3d>();
-			if (countBelow != 3 && countAbove != 3) {
-
-				for (Vec3d vertex : t.getVertices()) {
-					// if they're on opposite sides
-					if ((zThresh - vertex.z) * (zThresh - prev.z) <= 0) {
-						list.add(getIntersect(vertex, prev, zThresh));
-					}
-					if ((vertex.z - zThresh) * (vertex.z - zThresh) <= almostZero) {
-						// not sure this is the right thing to do for the "equals" case
-						list.add(vertex);
-					}
-					prev = vertex;
-				}
-				if (list.size() < 2) {
-					System.out.println(countBelow + " " + zThresh);
-					System.out.println(t);
-				}
-			}
-			addToContour(list);
-		}
-		return contour;
-	}
-
-	private void addToContour(List<Vec3d> list) {
-		if (list.isEmpty()) {
-			return;
-		}
-		contour.moveTo(list.get(0).x, list.get(0).y);
-		for (Vec3d pt : list) {
-			if (!pt.equals(list.get(0))) {
-				contour.lineTo(pt.x, pt.y);
-				return;
-			}
-		}
-	}
-
-	private Vec3d getIntersect(Vec3d pt1, Vec3d pt2, double zThresh) {
-		double scale = (zThresh - pt1.z) / (pt2.z - pt1.z);
-		double newX = pt1.x + scale * (pt2.x - pt1.x);
-		double newY = pt1.y + scale * (pt2.y - pt1.y);
-
-		return new Vec3d(newX, newY, zThresh);
-	}
-
-	private Point2D.Double convert(Vec3d pt) {
-		return new Point2D.Double(pt.x, pt.y);
-	}
-
 	private class ZoomListener implements MouseWheelListener {
 
 		@Override
 		public void mouseWheelMoved(MouseWheelEvent arg0) {
-			// negative values if the mouse wheel was rotated up/away from the user, and
-			// positive values if the mouse wheel was rotated down/ towards the user
-			int rot = arg0.getWheelRotation();
+			// negative values if the mouse wheel was rotated up/away from the
+			// user, and
+			// positive values if the mouse wheel was rotated down/ towards the
+			// user
+			int rot = -arg0.getWheelRotation();
 			double scale = Math.pow(1.1, rot);
 			// System.out.println(rot + " " + scale);
 
@@ -232,15 +135,15 @@ public class ContourCreator extends JPanel {
 			zoom.translate(arg0.getX() - scale * arg0.getX(), arg0.getY() - scale * arg0.getY());
 
 			List<Shape> newShapes = new ArrayList<Shape>();
-			for (Shape co : transformedContours) {
+			for (Shape co : contours) {
 				newShapes.add(zoom.createTransformedShape(co));
 			}
-			transformedContours = newShapes;
+			contours = newShapes;
 			newShapes = new ArrayList<Shape>();
-			for (Shape co : transformedMajorContours) {
+			for (Shape co : majorContours) {
 				newShapes.add(zoom.createTransformedShape(co));
 			}
-			transformedMajorContours = newShapes;
+			majorContours = newShapes;
 			repaint();
 		}
 
@@ -248,23 +151,21 @@ public class ContourCreator extends JPanel {
 
 	private class DragListener implements MouseMotionListener {
 
-		Point2D.Double pt = new Point2D.Double(0, 0);
-
 		@Override
 		public void mouseDragged(MouseEvent arg0) {
 			// drag.translate(arg0.getX() - pt.x, arg0.getY() - pt.y);
 			drag.setToTranslation(arg0.getX() - pt.x, arg0.getY() - pt.y);
 
 			List<Shape> newShapes = new ArrayList<Shape>();
-			for (Shape co : transformedContours) {
+			for (Shape co : contours) {
 				newShapes.add(drag.createTransformedShape(co));
 			}
-			transformedContours = newShapes;
+			contours = newShapes;
 			newShapes = new ArrayList<Shape>();
-			for (Shape co : transformedMajorContours) {
+			for (Shape co : majorContours) {
 				newShapes.add(drag.createTransformedShape(co));
 			}
-			transformedMajorContours = newShapes;
+			majorContours = newShapes;
 
 			pt.setLocation(arg0.getX(), arg0.getY());
 			repaint();
@@ -274,8 +175,29 @@ public class ContourCreator extends JPanel {
 		public void mouseMoved(MouseEvent arg0) {
 			// TODO Auto-generated method stub
 			pt.setLocation(arg0.getX(), arg0.getY());
+			repaint();
 		}
 
+	}
+	
+	public void drawWithColor(Graphics2D g, Color color, Point2D.Double pt) {
+		Color c = g.getColor();
+		g.setColor(color);
+		AffineTransform at = g.getTransform();
+
+		// half of final width/height in pixels
+		double crossWidth = 3, x=pt.x, y=pt.y;
+
+		// change so affine transform doesn't ruin
+		double xDiff = crossWidth / at.getScaleX(), yDiff = crossWidth
+				/ at.getScaleY();
+
+		// draw
+		Line2D.Double l = new Line2D.Double(x - xDiff, y, x + xDiff, y);
+		g.draw(l);
+		l = new Line2D.Double(x, y - yDiff, x, y + yDiff);
+		g.draw(l);
+		g.setColor(c);
 	}
 
 }
---
+++ src/TopoMap.java
@@ -0,0 +1,203 @@
+import java.awt.geom.Path2D;
+import java.awt.geom.Point2D;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class TopoMap {
+
+	private List<Triangle> mesh;
+	private Map<Double, Set<List<Point2D.Double>>> contours = new TreeMap<Double, Set<List<Point2D.Double>>>();
+	private double lakeWashThresh = 4.06;
+
+	private static int foundMatch = 0;
+
+	public TopoMap(String file) throws IOException {
+
+		Path stlPath = Paths.get(file);
+
+		mesh = STLParser.parseSTLFile(stlPath);
+	}
+
+	public double[][] getBounds() {
+		double[] min = { Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE };
+		double[] max = { Double.MIN_VALUE, Double.MIN_VALUE, Double.MIN_VALUE };
+		for (Triangle t : mesh) {
+			for (Vec3d vertex : t.getVertices()) {
+				double[] array = getArray(vertex);
+				for (int i = 0; i < 3; i++) {
+					if (array[i] < min[i]) {
+						min[i] = array[i];
+					}
+					if (array[i] > max[i]) {
+						max[i] = array[i];
+					}
+				}
+			}
+		}
+		return new double[][] { min, max };
+	}
+
+	private double[] getArray(Vec3d pt) {
+		return new double[] { pt.x, pt.y, pt.z };
+	}
+
+	public void initialize(double[] thresholds) {
+		contours.clear();
+		for (double threshold : thresholds) {
+			contours.put(threshold, process(threshold));
+		}
+	}
+
+	public void initialize(double min, double max, double step) {
+		contours.clear();
+		for (double threshold = min; threshold < max; threshold += step) {
+			contours.put(threshold, process(threshold));
+		}
+	}
+
+	private Set<List<Point2D.Double>> process(double zThresh) {
+		Map<Point2D.Double, List<Point2D.Double>> consolidator = new HashMap<Point2D.Double, List<Point2D.Double>>();
+		// in case you need to change double equality
+		double almostZero = 0.0000;
+
+		for (Triangle t : mesh) {
+			int countBelow = 0, countAbove = 0, countAt = 0;
+			Vec3d prev = null;
+
+			// special Lake Washington filter
+			if (zThresh == lakeWashThresh) {
+				if (t.getVertices()[0].x < 110 || //
+						(t.getVertices()[0].x < 140 && t.getVertices()[0].y < 50)) {
+					continue;
+				}
+			}
+
+			// figure out whether this triangle intersects the z = zThresh plane
+			for (Vec3d vertex : t.getVertices()) {
+				if ((vertex.z - zThresh) * (vertex.z - zThresh) <= almostZero) {
+					// not sure this is the right thing to do for the "equals"
+					// case
+					countAt++;
+				} else if (vertex.z < zThresh) {
+					countBelow++;
+				} else {
+					countAbove++;
+				}
+				prev = vertex;
+			}
+
+			List<Point2D.Double> list = new ArrayList<Point2D.Double>();
+			// if the triangle intersects the plane...
+			if (countBelow != 3 && countAbove != 3) {
+				// check for consecutive vertices on opposite sides of the plane
+				for (Vec3d vertex : t.getVertices()) {
+					// if they're on opposite sides
+					if ((zThresh - vertex.z) * (zThresh - prev.z) <= 0) {
+						list.add(getIntersect(vertex, prev, zThresh));
+					}
+					if ((vertex.z - zThresh) * (vertex.z - zThresh) <= almostZero) {
+						// not sure this is the right thing to do for the
+						// "equals" case
+						list.add(new Point2D.Double(vertex.x, vertex.y));
+					}
+					prev = vertex;
+				}
+			}
+			addToContour(list, consolidator);
+		}
+		Set<List<Point2D.Double>> contour = new HashSet<List<Point2D.Double>>();
+		contour.addAll(consolidator.values());
+
+		return contour;
+	}
+
+	/**
+	 * List of paths corresponding to threshold values
+	 * 
+	 * @return
+	 */
+	public List<Path2D.Double> asPaths() {
+		List<Path2D.Double> paths = new ArrayList<Path2D.Double>();
+		for (double thresh : contours.keySet()) {
+			Set<List<Point2D.Double>> curContours = contours.get(thresh);
+			Path2D.Double path2d = new Path2D.Double();
+			for (List<Point2D.Double> path : curContours) {
+				path2d.moveTo(path.get(0).x, path.get(0).y);
+				for (Point2D.Double pt : path) {
+					path2d.lineTo(pt.x, pt.y);
+				}
+			}
+			paths.add(path2d);
+		}
+		return paths;
+	}
+
+	public static void addToContour(List<Point2D.Double> l0, Map<Point2D.Double, List<Point2D.Double>> consolidator) {
+		if (l0.isEmpty()) {
+			return;
+		}
+
+		// beginning state:
+		// l0: list(0) .... list(end)
+		// l1: list(0) .... l1(end) (or l1: null) (or:
+		// l1: l1(end) .... list(0)
+		for (int i = 0; i < 2; i++) {
+			Point2D.Double cur = l0.get(0);
+			List<Point2D.Double> l1 = consolidator.get(cur);
+			if (l1 != null) {
+				foundMatch++;
+			}
+			if (l0.equals(l1))
+				continue;
+			if (l1 != null) {
+				// remove l1 from the map for now
+				consolidator.remove(l1.get(0));
+				consolidator.remove(l1.get(l1.size() - 1));
+
+				if (l1.get(0).equals(cur))
+					Collections.reverse(l1);
+
+				// at this point, we have:
+				// l0: list(0) .... list(end)
+				// l1: l1(end) .... list(0)
+				l0.remove(0);
+				l1.addAll(l0);
+				l0 = l1;
+
+				// at this point, we have:
+				// l0: l1(end) .... list(0) .... list(end)
+
+				// after the next reverse, we get:
+				// l0: list(end) .... list(0) .... l1(end)
+				// this means that on the second pass through the loop we're
+				// looking for a match of list(end)
+				// and it should still be in the map unless that closed a loop
+				// but in the case of a closed loop at this point both ends of
+				// the list will be that endpoint
+			}
+			Collections.reverse(l0);
+		}
+
+		consolidator.put(l0.get(0), l0);
+		consolidator.put(l0.get(l0.size() - 1), l0);
+	}
+
+	private Point2D.Double getIntersect(Vec3d pt1, Vec3d pt2, double zThresh) {
+		double scale = (zThresh - pt1.z) / (pt2.z - pt1.z);
+		double newX = pt1.x + scale * (pt2.x - pt1.x);
+		double newY = pt1.y + scale * (pt2.y - pt1.y);
+
+		return new Point2D.Double(newX, newY);
+	}
+}
