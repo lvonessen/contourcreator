f959849fcf8eda5f707eaeff40f211c3f97aa8d0
Laura Vonessen <laurav4@cs.washington.edu>
2018-05-27 13:05:26 -0700

    AffineTransforms are gnarly but I think I defeated them.

--- src/ContourCreator.java
+++ src/ContourCreator.java
@@ -5,13 +5,16 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Shape;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseWheelEvent;
 import java.awt.event.MouseWheelListener;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Line2D;
+import java.awt.geom.NoninvertibleTransformException;
 import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
+import java.awt.geom.Point2D.Double;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,10 +27,11 @@ public class ContourCreator extends JPanel {
 
 	private List<Shape> contours;
 	private List<Shape> majorContours;
-	private AffineTransform zoom = new AffineTransform();
-	private AffineTransform drag = new AffineTransform();
-	
-	Point2D.Double pt = new Point2D.Double(0, 0);
+	// private AffineTransform zoom = new AffineTransform();
+	// private AffineTransform drag = new AffineTransform();
+	private AffineTransform at = new AffineTransform();
+
+	Point2D.Double mouseLocation = new Point2D.Double(0, 0);
 
 	private double lakeWashThresh = 4.06;
 	private double[] seattleMajorVals = { 3, //
@@ -48,6 +52,17 @@ public class ContourCreator extends JPanel {
 		f.pack();
 		f.setSize(new Dimension(700, 900));
 		f.setVisible(true);
+
+		AffineTransform at = new AffineTransform();
+
+		at.scale(1, 2);
+		System.out.println(at);
+		at.translate(3, 4);
+		System.out.println(at);
+		at.scale(5, 6);
+		System.out.println(at);
+		at.translate(7, 8);
+		System.out.println(at);
 
 	}
 
@@ -93,21 +108,25 @@ public class ContourCreator extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		Graphics2D g2 = (Graphics2D) g;
+		// AffineTransform atOrig = g2.getTransform();
+		// g2.transform(at);
 
 		boolean col = true;
 		g2.setColor(Color.LIGHT_GRAY);
 		for (Shape co : contours) {
-			g2.draw(co);// zoom.createTransformedShape(drag.createTransformedShape(co)));
+			g2.draw(at.createTransformedShape(co));// zoom.createTransformedShape(drag.createTransformedShape(co)));
 		}
 		g2.setColor(Color.BLACK);
 		g2.setStroke(new BasicStroke(2));
 		for (int i = 0; i < majorContours.size(); i++) {
 			Shape co = majorContours.get(i);
 			g2.setColor(seattleColors[i]);
-			g2.draw(co);
+			g2.draw(at.createTransformedShape(co));
 		}
-		
-		drawWithColor(g2, Color.BLACK, pt);
+
+		drawWithColor(g2, Color.BLACK, mouseLocation);
+
+		// g2.setTransform(atOrig);
 	}
 
 	private Color toggle(boolean tog) {
@@ -130,21 +149,62 @@ public class ContourCreator extends JPanel {
 			double scale = Math.pow(1.1, rot);
 			// System.out.println(rot + " " + scale);
 
-			// does the order have to be reversed here?
-			zoom.setToScale(scale, scale);
-			zoom.translate(arg0.getX() - scale * arg0.getX(), arg0.getY() - scale * arg0.getY());
+			// no scaling, no shifting
+			Point2D.Double pt1 = null;
+			try {
+				pt1 = (Double) at.inverseTransform(mouseLocation, null);
+			} catch (NoninvertibleTransformException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
-			List<Shape> newShapes = new ArrayList<Shape>();
-			for (Shape co : contours) {
-				newShapes.add(zoom.createTransformedShape(co));
-			}
-			contours = newShapes;
-			newShapes = new ArrayList<Shape>();
-			for (Shape co : majorContours) {
-				newShapes.add(zoom.createTransformedShape(co));
-			}
-			majorContours = newShapes;
+			at.scale(scale, scale);
+
+			Point2D.Double pt2 = (Double) at.transform(pt1, null);
+
+			at.translate((-pt2.x + mouseLocation.x) / at.getScaleX(), (-pt2.y + mouseLocation.y) / at.getScaleY());
+
+			// zoom.setToScale(scale, scale);
+			// zoom.translate(arg0.getX() - scale * arg0.getX(), arg0.getY() -
+			// scale * arg0.getY());
+			// at.translate(arg0.getX() - scale * arg0.getX(), arg0.getY() -
+			// scale * arg0.getY());
+
 			repaint();
+		}
+
+	}
+
+	private class ClickListener implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+
 		}
 
 	}
@@ -154,43 +214,46 @@ public class ContourCreator extends JPanel {
 		@Override
 		public void mouseDragged(MouseEvent arg0) {
 			// drag.translate(arg0.getX() - pt.x, arg0.getY() - pt.y);
-			drag.setToTranslation(arg0.getX() - pt.x, arg0.getY() - pt.y);
+			// drag.setToTranslation
+			at.translate((arg0.getX() - mouseLocation.x) / at.getScaleX(),
+					(arg0.getY() - mouseLocation.y) / at.getScaleY());
 
-			List<Shape> newShapes = new ArrayList<Shape>();
-			for (Shape co : contours) {
-				newShapes.add(drag.createTransformedShape(co));
-			}
-			contours = newShapes;
-			newShapes = new ArrayList<Shape>();
-			for (Shape co : majorContours) {
-				newShapes.add(drag.createTransformedShape(co));
-			}
-			majorContours = newShapes;
+			// List<Shape> newShapes = new ArrayList<Shape>();
+			// for (Shape co : contours) {
+			// newShapes.add(drag.createTransformedShape(co));
+			// }
+			// contours = newShapes;
+			// newShapes = new ArrayList<Shape>();
+			// for (Shape co : majorContours) {
+			// newShapes.add(drag.createTransformedShape(co));
+			// }
+			// majorContours = newShapes;
+			//
+			// pt.setLocation(arg0.getX(), arg0.getY());
 
-			pt.setLocation(arg0.getX(), arg0.getY());
+			mouseLocation.setLocation(arg0.getX(), arg0.getY());
 			repaint();
 		}
 
 		@Override
 		public void mouseMoved(MouseEvent arg0) {
 			// TODO Auto-generated method stub
-			pt.setLocation(arg0.getX(), arg0.getY());
+			mouseLocation.setLocation(arg0.getX(), arg0.getY());
 			repaint();
 		}
 
 	}
-	
+
 	public void drawWithColor(Graphics2D g, Color color, Point2D.Double pt) {
 		Color c = g.getColor();
 		g.setColor(color);
 		AffineTransform at = g.getTransform();
 
 		// half of final width/height in pixels
-		double crossWidth = 3, x=pt.x, y=pt.y;
+		double crossWidth = 3, x = pt.x, y = pt.y;
 
 		// change so affine transform doesn't ruin
-		double xDiff = crossWidth / at.getScaleX(), yDiff = crossWidth
-				/ at.getScaleY();
+		double xDiff = crossWidth / at.getScaleX(), yDiff = crossWidth / at.getScaleY();
 
 		// draw
 		Line2D.Double l = new Line2D.Double(x - xDiff, y, x + xDiff, y);
